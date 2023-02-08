cd07aa3f0bfb462dcf3d59fcd28029b792dbc9ab
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-02-08 12:53:06 +0300

    Add update and delete methods in LinkController + views for them.

--- src/DataSaver/Controllers/LinkController.cs
+++ src/DataSaver/Controllers/LinkController.cs
@@ -72,5 +72,83 @@
             }
         }
 
+        [HttpGet]
+        public async Task<IActionResult> UpdateLink(int id)
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var topics = await _topicService.GetAllAsync();
+
+            LinkForCreateViewModel model = new()
+            {
+                LinkVM = await _linkService.GetByIdAsync(id),
+
+                Categories = categories.Select(_ => new SelectListItem
+                {
+                    Value = _.Id.ToString(),
+                    Text = _.Name
+                }),
+
+                Topics = topics.Select(_ => new SelectListItem
+                {
+                    Value = _.Id.ToString(),
+                    Text = _.Name
+                })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateLink(LinkForCreateViewModel linkForCreateVM)
+        {
+            var modelForUpdate = linkForCreateVM.LinkVM;
+
+            if (ModelState.IsValid)
+            {
+                var updatedLink = await _linkService.UpdateAsync(modelForUpdate!);
+
+                return RedirectToAction("Index");
+            }
+
+            else return View(linkForCreateVM);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteLink(int id)
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var topics = await _topicService.GetAllAsync();
+
+            LinkForCreateViewModel model = new()
+            {
+                LinkVM = await _linkService.GetByIdAsync(id),
+
+                Categories = categories.Select(_ => new SelectListItem
+                {
+                    Value = _.Id.ToString(),
+                    Text = _.Name
+                }),
+
+                Topics = topics.Select(_ => new SelectListItem
+                {
+                    Value = _.Id.ToString(),
+                    Text = _.Name
+                })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteLink(LinkForCreateViewModel linkToRemoveVM)
+        {
+            var linkToRemove = await _linkService.GetByIdAsync(linkToRemoveVM.LinkVM!.Id);
+            await _linkService.DeleteAsync(linkToRemove);
+
+            return RedirectToAction("Index");
+
+        }
     }
 }
