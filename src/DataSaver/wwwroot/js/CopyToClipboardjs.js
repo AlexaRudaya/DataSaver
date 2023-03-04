2aad79b378137b8821e20380ce4ee1e2d7ec9342
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-03-04 20:44:37 +0300

    Adding  a copy to clipboard function

---
+++ src/DataSaver/wwwroot/js/CopyToClipboardjs.js
@@ -0,0 +1,17 @@
+﻿function copyToClipboard(url) {
+    // Create a temporary input element
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = url;
+    document.body.appendChild(input);
+
+    // Select the input element's text
+    input.select();
+    input.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text to the clipboard
+    document.execCommand('copy');
+
+    // Remove the temporary input element
+    document.body.removeChild(input);
+}
