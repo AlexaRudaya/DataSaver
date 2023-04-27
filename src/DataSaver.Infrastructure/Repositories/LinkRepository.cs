cab0c1d9062cc3aed60a7e6e3e4bcdd90419bc3b
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-04-27 14:06:01 +0300

    Sorting and filtering fixing.

--- src/DataSaver.Infrastructure/Repositories/LinkRepository.cs
+++ src/DataSaver.Infrastructure/Repositories/LinkRepository.cs
@@ -9,31 +9,31 @@
             _linkContext = linkContext;
         }
 
-        public async Task<IEnumerable<Link>> GetAllByFilterAsync(
-            Func<IQueryable<Link>, IIncludableQueryable<Link, object>>? include = null,
-            params Expression<Func<Link, bool>>[] expressions)
-        {
-            IQueryable<Link> query = _linkContext.Links;
+        //public async Task<IEnumerable<Link>> GetAllByFilterAsync(
+        //    Func<IQueryable<Link>, IIncludableQueryable<Link, object>>? include = null,
+        //    params Expression<Func<Link, bool>>[] expressions)
+        //{
+        //    IQueryable<Link> query = _linkContext.Links;
 
-            var expressionsList = expressions.ToList();
-            expressionsList.ForEach(_ => query = query.Where(_));
+        //    var expressionsList = expressions.ToList();
+        //    expressionsList.ForEach(_ => query = query.Where(_));
 
-            if (include is not null)
-            {
-                query = include(query);
-            } 
+        //    if (include is not null)
+        //    {
+        //        query = include(query);
+        //    } 
 
-            return await query.AsNoTracking().ToListAsync();
-        }
+        //    return await query.AsNoTracking().ToListAsync();
+        //}
 
-        public async Task<IEnumerable<Link>> GetAllSortedAsync(
+        public async Task<IEnumerable<Link>> GetAllByAsync(
             Func<IQueryable<Link>, IIncludableQueryable<Link, object>>? include = null, 
-            params Expression<Func<Link, object>>[] sortExpressions)
+            Expression<Func<Link, object>> sortExpression = null, params Expression<Func<Link, object>>[] filterExpressions)
         {
             IQueryable<Link> query = _linkContext.Links;
 
             var sortExpressionsList = sortExpressions.ToList();
-            sortExpressionsList.ForEach(_ => query = query.OrderBy(_));
+            //sortExpressionsList.ForEach(_ => query = query.OrderByDescending(_));
 
             if (include is not null)
             {
@@ -42,5 +42,38 @@
 
             return await query.AsNoTracking().ToListAsync();
         }
+        private IQueryable<Link> GetAllByFilterAsync(
+           Func<IQueryable<Link>, IIncludableQueryable<Link, object>>? include = null,
+           params Expression<Func<Link, bool>>[] expressions)
+        {
+            IQueryable<Link> query = _linkContext.Links;
+
+            if (include is not null)
+            {
+                query = include(query);
+            }
+
+            var expressionsList = expressions.ToList();
+            expressionsList.ForEach(_ => query = query.Where(_));
+
+            return query;
+        }
+
+        private IQueryable<Link> GetAllBySortAsync(
+           Func<IQueryable<Link>, IIncludableQueryable<Link, object>>? include = null,
+           Expression<Func<Link, bool>> expression)
+        {
+            IQueryable<Link> query = _linkContext.Links;
+
+            if (include is not null)
+            {
+                query = include(query);
+            }
+
+            query = query.OrderBy(expression);          
+           
+            return query;
+        }
+
     }
 }
